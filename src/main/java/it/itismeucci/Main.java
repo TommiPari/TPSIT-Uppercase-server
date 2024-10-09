5c6a5b65a4773290b9d069c14140b50f331a8383
Tommaso Parigi <tommaso.parigi06@gmail.com>
2024-10-09 09:14:19 +0200

    Comunicazione col client

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -1,13 +1,28 @@
 package it.itismeucci;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.println("Il server è partito!");
         ServerSocket ss = new ServerSocket(3000);
         Socket s = ss.accept();
         System.out.println("Qualcuno si è collegato!!");
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("Stringa ricevuta: " + stringaRicevuta);
+
+        out.writeBytes("Ciao sono il server!\n");
+
+        s.close();
+        ss.close();
     }
 }
